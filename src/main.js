e51ec231c2b7998d0524b2fbbc2c0e5f2e89aa0f
miasK3011 <neemiascalebe2014@gmail.com>
2022-10-19 14:32:27 -0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
-import "./css/index.css"
+import "./css/index.css";
+
+const ccBgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBgColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+ccBgColor1.setAttribute("fill", "green");
+ccBgColor2.setAttribute("fill", "blue");
+
+const colors = {
+  visa: ["#2D57F2", "#436D99"],
+  mastercard: ["#C69347", "#DF6F29"],
+  default: ["black", "gray"],
+};
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#2D57F2", "#436D99"],
+    mastercard: ["#C69347", "#DF6F29"],
+    default: ["black", "gray"],
+  };
+
+  ccBgColor1.setAttribute("fill", colors[type][0]);
+  ccBgColor2.setAttribute("fill", colors[type][1]);
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+setCardType("mastercard")
+
+globalThis.setCardType = setCardType
